7d10426de2952d9f9494172cdaa732a960f29692
parkjkwan <pakjkwan@gmail.com>
2016-06-18 16:53:50 +0900

    16.06.18. 영화 포스터 코딩 중 (2)....

--- app/src/main/java/com/hanbit/user/myapp2/movie/MovieActivity.java
+++ app/src/main/java/com/hanbit/user/myapp2/movie/MovieActivity.java
@@ -1,18 +1,21 @@
 package com.hanbit.user.myapp2.movie;
 
 import android.app.Activity;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.GridView;
 import android.widget.ImageView;
 
 import com.hanbit.user.myapp2.R;
 
-public class MovieActivity extends Activity {
+public class MovieActivity extends Activity implements View.OnClickListener {
     GridView gv;
     MovieGridAdapter adapter;
+    // final int pos;
     Integer[] posterID = {
             R.drawable.mov01,R.drawable.mov02,R.drawable.mov03,
             R.drawable.mov04,R.drawable.mov05,R.drawable.mov06,
@@ -27,6 +30,8 @@ public class MovieActivity extends Activity {
         gv = (GridView) findViewById(R.id.gv);
         adapter = new MovieGridAdapter(this);
         gv.setAdapter(adapter);
+        findViewById(R.id.ivPoster).setOnClickListener(this);
+
     }
     public int getCount(){
         return posterID.length;
@@ -35,8 +40,35 @@ public class MovieActivity extends Activity {
         ImageView iv = new ImageView(MovieActivity.this);
         iv.setLayoutParams(new GridView.LayoutParams(100,150));
         iv.setScaleType(ImageView.ScaleType.FIT_CENTER);
+        iv.setPadding(5,5,5,5);
 
         iv.setImageResource(posterID[pos]);
         return iv;
     }
+
+    @Override
+    public void onClick(View v) {
+        View dialogView = View.inflate(MovieActivity.this, R.layout.dialog, null);
+        AlertDialog.Builder dlg = new AlertDialog.Builder(MovieActivity.this);
+        ImageView ivPoster = (ImageView) dialogView.findViewById(R.id.ivPoster);
+        ivPoster.setImageResource(posterID[0]);
+        dlg.setTitle("큰포스터");
+        // dlg.setIcon(R.drawable.ic)
+        dlg.setView(dialogView);
+        dlg.setNegativeButton("닫기",null);
+        dlg.show();
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
