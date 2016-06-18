821e2f6f31a014b7be111af56c07ddc1172a547a
parkjkwan <pakjkwan@gmail.com>
2016-06-18 11:10:36 +0900

    reboot

---
+++ app/src/main/java/com/hanbit/user/myapp2/group/GroupActivity.java
@@ -0,0 +1,15 @@
+package com.hanbit.user.myapp2.group;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.hanbit.user.myapp2.R;
+
+public class GroupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_group);
+    }
+}
